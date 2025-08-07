d175897f56533606d106ad87466ecbefc44695ac
1anderj <176311604+1anderj@users.noreply.github.com>
2025-08-07 11:13:05 -0400

    dropdown 2

--- assets/scripts/dropdowns.js
+++ assets/scripts/dropdowns.js
@@ -1,50 +1,21 @@
-function dropdownLists() {
-  document.getElementById("lists-dropdown").classList.toggle("show");
-}
-
-window.onclick = function(event) {
-  if (!event.target.matches('.menu')) {
-    var dropdowns = document.getElementsByClassName("dropdown-links");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+function toggleDropdown(id) {
+  // Close all dropdowns
+  var dropdowns = document.getElementsByClassName("dropdown-links");
+  for (var i = 0; i < dropdowns.length; i++) {
+    if (dropdowns[i].id !== id) {
+      dropdowns[i].classList.remove("show");
     }
   }
-}
 
-function dropdownInteractive() {
-  document.getElementById("interactive-dropdown").classList.toggle("show");
-}
-
-window.onclick = function(event) {
-  if (!event.target.matches('.menu')) {
-    var dropdowns = document.getElementsByClassName("dropdown-links");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
-function dropdownAbout() {
-  document.getElementById("about-dropdown").classList.toggle("show");
+  // Toggle the requested dropdown
+  document.getElementById(id).classList.toggle("show");
 }
 
 window.onclick = function(event) {
   if (!event.target.matches('.menu')) {
     var dropdowns = document.getElementsByClassName("dropdown-links");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+    for (var i = 0; i < dropdowns.length; i++) {
+      dropdowns[i].classList.remove("show");
     }
   }
 }
